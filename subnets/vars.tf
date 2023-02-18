9514e1951700d29cea8f76ac28c23f9abcb9ac8f
pavani <pavanikakarlapudi1990@gmail.com>
2023-02-18 22:41:07 +0530

    frontend jenkins

---
+++ subnets/vars.tf
@@ -0,0 +1,9 @@
+variable "cidr_block" {}
+variable "availability_zone" {}
+variable "vpc_id" {}
+variable "env" {}
+variable "default_vpc_id" {}
+variable "name" {}
+variable "vpc_peering_connection_id" {}
+variable "internet_gw" {}
+variable "nat_gw" {}
